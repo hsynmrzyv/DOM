70d4f0fd843ebb40b61b76cb7cbde7f55741bd04
HSYN <huseyn.mirzayev@code.edu.az>
2023-12-13 15:54:58 +0400

    finish task

---
+++ Lesson/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// console.log(Math.floor(Math.random() * 10 + 1));
+
+// Math.floor(Math.random() * (max - min) + min);
+
+// let age = 19;
+
+// age >= 18 ? console.log("You are adult") : console.log("You are not adult");
+
+// let isAdult = age >= 18 ? true : false;
